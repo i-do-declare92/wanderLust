4dd22feec60f9404a20c93c2893916176513d7f7
i-do-declare92 <steph_stephen92@outlook.com>
2023-08-02 03:43:41 +0000

    Carousel 95% completed

--- main.js
+++ main.js
@@ -6,35 +6,64 @@ const cards = document.querySelectorAll('.card1');
 const prevButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
 const cardWidth = cards[0].offsetWidth; // Get the width of the cards
+const numCards = cards.length;
+const numVisibleCards = 4; // Number of visible cards in the carousel
+let currentIndex = Math.floor((numCards - numVisibleCards) / 1);
+let isCarouselPaused = false;
 
-let currentIndex = 0;
+
 
 // Function to slide the carousel
 function slideCarousel() {
   const offset = -currentIndex * cardWidth;
   carousel.style.transition = 'transform 0.3s ease';
   carousel.style.transform = `translateX(${offset}px)`;
+
+  // Reset carousel to the first card when reaching the end
+  if (currentIndex === numCards) {
+    setTimeout(() => {
+      carousel.style.transition = 'none';
+      carousel.style.transform = `translateX(${-(numVisibleCards * cardWidth)}px)`;
+      currentIndex = numCards - numVisibleCards;
+    }, 300);
+  }
+
+  // Reset carousel to the last card when reaching the beginning
+  if (currentIndex === -1) {
+    setTimeout(() => {
+      carousel.style.transition = 'none';
+      carousel.style.transform = `translateX(${-(numCards - 1) * cardWidth}px)`;
+      currentIndex = numCards - 1;
+    }, 300);
+  }
 }
 
 // Function to handle previous button click
 function showPreviousCard() {
-  if (currentIndex > 0) {
-    currentIndex--;
-    slideCarousel();
-  }
+  currentIndex = (currentIndex - 1);
+  slideCarousel();
 }
 
 // Function to handle next button click
 function showNextCard() {
-  if (currentIndex < cards.length - 1) {
-    currentIndex++;
-    slideCarousel();
-  }
+  currentIndex = (currentIndex + 1);
+  slideCarousel();
 }
+
 
 // Add click event listeners to the navigation buttons
 prevButton.addEventListener('click', showPreviousCard);
 nextButton.addEventListener('click', showNextCard);
+
+function resumeCarousel() {
+  isCarouselPaused = false;
+  setTimeout(slideCarousel, 1000); // Resume sliding after 5 seconds
+}
+
+slideCarousel();
+setTimeout(resumeCarousel, 1000)
+
+
 //Reveals the more info button
 cards.forEach(card => {
     const revealButton = card.querySelector('.reveal-btn');
